70f8c34ed9f39adef19d7c2a1ed93c3b3b02b806
unknown <caovannhan2002@gmail.com>
2015-03-29 22:51:38 +0700

    FB: commit source

--- app/src/main/java/cvnhan/android/androidlogin/MainActivity.java
+++ app/src/main/java/cvnhan/android/androidlogin/MainActivity.java
@@ -3,10 +3,15 @@ package cvnhan.android.androidlogin;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender.SendIntentException;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,6 +21,21 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.widget.ShareDialog;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.SignInButton;
@@ -28,6 +48,9 @@ import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
 
 import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 
 public class MainActivity extends Activity implements OnClickListener,
@@ -58,9 +81,19 @@ public class MainActivity extends Activity implements OnClickListener,
     private TextView txtName, txtEmail;
     private LinearLayout llProfileLayout;
 
+
+    //facebook
+    private LoginButton loginButton;
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+
         setContentView(R.layout.activity_main);
         btnSignIn = (SignInButton) findViewById(R.id.btn_sign_in);
         btnSignOut = (Button) findViewById(R.id.btn_sign_out);
@@ -79,7 +112,103 @@ public class MainActivity extends Activity implements OnClickListener,
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this).addApi(Plus.API)
                 .addScope(Plus.SCOPE_PLUS_LOGIN).build();
+
+        //////////////////////////////////////////////////////////FACEBOOK
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo("cvnhan.android.androidlogin",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("namenotfound", e.toString());
+
+        } catch (NoSuchAlgorithmException e) {
+            Log.e("nosuchalgorithmex", e.toString());
+        }
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("email"));
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                ((TextView) findViewById(R.id.txtView)).setText(loginResult.getAccessToken().getUserId() + "-" + loginButton.getText());
+                Log.e("onSuccess", loginResult.getAccessToken().toString());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.e("onCancel", "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.e("onError", e.toString());
+            }
+        });
+        hasEmailPermission();
+        ((Button) findViewById(R.id.shareBtn)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (AccessToken.getCurrentAccessToken() != null) {
+                    ShareLinkContent content = new ShareLinkContent.Builder()
+                            .setContentUrl(Uri.parse("https://developers.facebook.com"))
+                            .build();
+                    ShareDialog.show(MainActivity.this, content);
+                }
+            }
+        });
+        findViewById(R.id.getMebtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(hasEmailPermission()) {
+                    final GraphRequest graphRequest = new GraphRequest(AccessToken.getCurrentAccessToken(), "/me", null, HttpMethod.GET, new GraphRequest.Callback() {
+                        @Override
+                        public void onCompleted(GraphResponse graphResponse) {
+                            String s = graphResponse.getJSONObject().toString();
+                            ((TextView) findViewById(R.id.infoTxt)).setText(s);
+                            Log.e("info", s);
+                        }
+                    });
+                    graphRequest.executeAsync();
+                }
+
+
+            }
+        });
+
+        shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+            @Override
+            public void onSuccess(Sharer.Result result) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+
     }
+    private boolean hasEmailPermission() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        if(accessToken==null) return false;
+        boolean check=accessToken.getPermissions().contains("email");
+        if(!check){
+            LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile", "email"));
+            return false;
+        }
+        return true;
+    }
+
 
     protected void onStart() {
         super.onStart();
@@ -144,6 +273,23 @@ public class MainActivity extends Activity implements OnClickListener,
                 mGoogleApiClient.connect();
             }
         }
+        callbackManager.onActivityResult(requestCode, responseCode, intent);
+        Log.e("callbackManager", requestCode + " " + responseCode + " " + intent.toString());
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 
     @Override
